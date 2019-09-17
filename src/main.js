c65f378b5822a978a6a21a8ec6002b3a48a69e0a
tony <xiaonengwu@kemai.cn>
2019-09-17 17:45:27 +0800

    '完善代码'

--- src/main.js
+++ src/main.js
@@ -11,22 +11,42 @@ let actionMap = {
         ]
     },
     config: {
-
+        alias: 'cfg',
+        description: 'config .xwz',
+        usages: [
+            'xwz config set <k> <v></v>',
+            'xwz config get <k>',
+            'xwz config remove <k>'
+        ]
     }
 }
 
 Object.keys(actionMap).forEach((action) => {
-
+    program.command(action).description(actionMap[action].description).alias(actionMap[action].alias).action(() => {
+        switch (action) {
+            case 'config':
+                apply(action, ...program.argv.slice(3));
+                break;
+            case 'init':
+                apply(action, ...program.argv.slice(3));
+                break;
+            default:
+                break;
+        }
+    });
 });
 
 function help () {
     console.log('\r\nUsage:');
     Object.keys(actionMap).forEach((action) => {
-
-    })
+        actionMap[action].usages.forEach(usage => {
+            console.log(' - ' + usage);
+        });
+    });
+    console.log('\r');
 }
 
-program.usage('');
+program.usage('<command> [options]');
 program.usage('-h', help);
 program.usage('--help', help);
 program.version(VERSION, '-V --version').parse(process.argv);
@@ -34,7 +54,7 @@ program.version(VERSION, '-V --version').parse(process.argv);
 // 不带参数时
 
 if (!process.argv.slice(2).length) {
-
+    program.outputHelp(make_green);
 }
 function make_green (txt) {
     return chalk.green(txt);
---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+import { version } from '../../package.json';
+
+// 当前package.json 的版本号
+export const VERSION = version;
+
+// 用户的根目录
+const HOME = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'];
+
+// 配置文件目录
+export const RC = `${HOME}/.xwz`;
+
+export const DEFAULTS = {
+    registry: '',
+    trype: 'users'
+}
