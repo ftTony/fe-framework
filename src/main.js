c11d5a6e6cf763d45eb16451c03f9f3a79ab8c55
tony <xiaonengwu@kemai.cn>
2019-09-17 17:45:15 +0800

    '完善代码'

--- src/main.js
+++ src/main.js
@@ -3,3 +3,40 @@ import { VERSION } from './utils/constants';
 import apply from './index';
 import chalk from 'chalk';
 
+let actionMap = {
+    init: {
+        description: '生成一个新项目模板',
+        usages: [
+            '给新项目模板起一个名字'
+        ]
+    },
+    config: {
+
+    }
+}
+
+Object.keys(actionMap).forEach((action) => {
+
+});
+
+function help () {
+    console.log('\r\nUsage:');
+    Object.keys(actionMap).forEach((action) => {
+
+    })
+}
+
+program.usage('');
+program.usage('-h', help);
+program.usage('--help', help);
+program.version(VERSION, '-V --version').parse(process.argv);
+
+// 不带参数时
+
+if (!process.argv.slice(2).length) {
+
+}
+function make_green (txt) {
+    return chalk.green(txt);
+}
+
