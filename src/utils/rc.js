62eead1c2e7ee02211d5577ca6fef9e9f0de9ecb
tony <xiaonengwu@kemai.cn>
2019-09-18 14:03:11 +0800

    '修改相关配置'

--- src/utils/rc.js
+++ src/utils/rc.js
@@ -3,3 +3,17 @@ import { decode, encode } from 'ini';
 import { promisify } from 'util';
 import chalk from 'chalk';
 import fs from 'fs';
+
+const exits = promisify(fs.exists);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+// RC 是配置文件
+// DEFAULTS 是默认的配置
+export const get = async (key) => {
+    const exit = await exits(RC);
+    let opts;
+    if (exit) {
+
+    }
+}
